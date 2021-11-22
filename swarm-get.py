592f6dae3ed1be344861835c8e3da83720f35ce5
Kirk Martinez <km@ecs.soton.ac.uk>
2021-11-22 11:34:11 +0000

    needed POST for ACK

--- swarm-get.py
+++ swarm-get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# get data and append to a file
+# get Swarm data and append to a file, with logs to a logfile
 from pprint import pprint
 import base64
 import json
@@ -20,9 +20,6 @@ loginURL = hiveBaseURL + '/login'
 getMessageURL = hiveBaseURL + '/api/v1/messages'
 ackMessageURL = hiveBaseURL + '/api/v1/messages/rxack/{}'
 
-# dont do the ACK
-doACK = False
-
 # create a session
 with requests.Session() as s:
     # log in to get the JSESSIONID cookie
@@ -30,12 +27,8 @@ with requests.Session() as s:
     #print(res.url)
 
 if res.status_code != 200:
-    print("Invalid username or password; please use a valid username and passwor
-d in loginParams.")
+    print("Invalid username or password; please use a valid username and password in loginParams.")
     exit(1)
-
-# print out the JSESSIONID cookie
-#print(s.cookies)
 
 # let the session manage the cookie and get the output for the given appID
 # only pull the last 10 items that have not been ACK'd
@@ -44,10 +37,27 @@ res = s.get(getMessageURL, headers=hdrs, params={'count': 10, 'status': 0})
 
 messages = res.json()
 
-# print out the prettied version of the JSON records for unacknowledge data reco
-rds in the hive
+# print out the prettied version of the JSON records for unacknowledge data records in the hive
 print("jason dumps")
 print(json.dumps(messages, indent=4))
 f = open(logfile, "a")
 f.write(json.dumps(messages, indent=4))
 f.close()
+
+print("data pair")
+f = open(datafile, "a")
+# for all the items in the json returned (limited to 10 above)
+for item in messages:
+    # if there is a 'data' keypair, output the data portion converting it from base64 to ascii - assumes not binary
+    if (item['data']):
+        decodedmsg = base64.b64decode(item['data']).decode('ascii') + '\n'
+        print(decodedmsg)
+        f.write(decodedmsg)
+        # ack it so its deleted on the hive
+        print("ACKing message", item['packetId'])
+        print(ackMessageURL.format(item['packetId']) )
+        res1 = s.post(ackMessageURL.format(item['packetId']), headers=hdrs)
+        pprint(res1.json())
+
+f.close()
+
